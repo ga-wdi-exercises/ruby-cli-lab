fd50338523c78013b62537f243138b2c32863b47
Mike Flores <michaelprflores@gmail.com>
2016-09-29 11:19:25 -0400

    Created a hash with k v pairs of alphabet and number

--- battleship/dev.rb
+++ battleship/dev.rb
@@ -1,4 +1,29 @@
+# Pseudocode
+# build the board
+# vertical and horizontal axis
+# use numbers for vertical and alphabet for horizontal
+# I can use enumberables to create a board like I did card deck
+# but I will manually create the board
+
+# create the board
+# create the battleships
+# create the player and what the player can do
+
 require 'pry'
+# using symbols cuz they faster
+board = {
+    :a => 1, 
+    :b => 2, 
+    :c => 3,
+    :d => 4,
+    :e => 5,
+    :f => 6,
+    :g => 7,
+    :h => 8,
+    :i => 9,
+    :j => 10
+}
+
 
 binding.pry
 puts "dunzo"
