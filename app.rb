4f98ac5e6c850147bd25a1a81d6256e20389dbed
Barrett Cope <barrettcope@gmail.com>
2016-09-29 15:12:04 -0400

    Draw board working

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require "pry"
 
+require_relative "models/grid"
 require_relative "models/square"
 require_relative "models/boat"
 
-
+grid = Grid.new "Barrett"
+puts grid
 
 binding.pry
 puts "done"
--- models/grid.rb
+++ models/grid.rb
@@ -1,26 +1,60 @@
+require_relative "square"
+require_relative "boat"
+
 class Grid
 
   @@all = []
   @@count = 0
 
-  attr_accessor
+  attr_accessor :id, :player, :squares, :boats
 
   def initialize player
-    @id = @@count + 1
+    @@count += 1
+    @id = @@count
     @player = player
-    @squares = []
-    create_squares
-    @boats = []
-    create_boats
+    @squares = create_squares
+    @boats = create_boats
     @@all << self
   end
 
-  def create_squares
+  def draw_border
+    "+" + ("---+" * 11)
+  end
 
+  def draw_row y
+    output = ""
+    r = Square.row y
+    r.each do |s|
+      # hardcoded show_boats to true
+      output += s.draw true
+    end
+    output
+  end
+
+  def draw_board
+    a = (0..10).to_a
+    puts draw_border
+    a.each do |yv|
+      puts draw_row yv
+      puts draw_border
+    end
+  end
+
+  def create_squares
+    x = (0..10).to_a
+    squares = []
+    x.each do |xv|
+      x.each do |yv|
+        squares << Square.new(xv,yv)
+      end
+    end
+    squares
   end
 
   def create_boats
-
+    boats = []
+    puts "not finished"
+    boats
   end
 
   def self.all
