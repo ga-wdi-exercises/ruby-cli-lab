eff71d93f0747dd2c5ec00762b9bb75f9ceedba8
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-29 15:35:34 -0400

    added if

--- flash-cards.rb
+++ flash-cards.rb
@@ -1,10 +1,28 @@
 require 'pry'
+class Flashcards
+  attr_accessor :front, :back
+
+  def initialize front, back
+    @front = front
+    @back = back
+  end
+end
+class Deck
+  attr_accessor :cards
+  def initialize
+    @cards = []
+  end
+end
+d = Deck.new
+d.cards.push(
+Flashcards.new("front", "back")
+)
 #create menu class for user to select option
 class Menu
   def self.display
-puts "Welcome to flashcards."
+    puts "Welcome to Antonym Flashcards."
     sleep 1.5
-    puts "Get ready to choose your option!"
+    puts "Enter the opposite meaning to win"
     sleep 1.5
     while 1
       puts "Choose one of the following:"
@@ -21,23 +39,42 @@ puts "Welcome to flashcards."
         puts "Invalid option."
       end
     end
-  end
-  def self.select number
-    puts "You selected #{number}"
+
+    def self.select number
+      puts "You selected #{number}"
+    end
+    if number == "1"
+      p "Welcome to all the Flashcards"
+      Flashcards.new
+    end
+    if number == "2"
+      p "Create new cards"
+      Create.new
+    end
+    if number == "3"
+      p "Edit current cards"
+      Edit.new
+    end
+    if number == "4"
+      p "Delete cards"
+      Delete.new
+    end
+    if number == "5"
+      p "View your score"
+      View.new
+    end
+
   end
 end
 
 Menu.display
 
 #create flash cards
-class Flashcards
-  attr_accessor :front, :back
+#need to create a txt file that will house the questions and answers
 
-  def initialize front, back
-    @front = front
-    @back = back
-  end
-end
+
+
+
 #create class to delete
 class Create < Flashcards
   def initialize create
@@ -58,11 +95,13 @@ class Delete < Flashcards
   end
 end
 #create score class
-class Score < Flashcards
+class View < Flashcards
   def initialize score
     @score = score
   end
 end
+
+
 
 binding.pry
 p "thursday"
