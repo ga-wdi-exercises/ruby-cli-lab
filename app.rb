b526680f7b7d4c675a2d96dc85dfc4fcd6c34b2e
Tim Foley <thetimfoley@gmail.com>
2016-09-29 13:05:47 -0400

    added expense adder to Month class

--- app.rb
+++ app.rb
@@ -6,8 +6,9 @@ require 'date'
 tim = User.new("Tim")
 jack = User.new("Jack")
 eggs = Expense.new(4, "eggs", "tim", "9/28/16", "groceries")
-milk = Expense.new(3, "milk", "tim", "9/26/16", "groceries")
+milk = Expense.new(3, "milk", "tim", "8/26/16", "groceries")
 bags = Expense.new(15, "trash bags", "jack", "9/26/16", "home")
+sept = Month.new(9, 2016)
 
 require "pry"
 binding.pry
--- models/month.rb
+++ models/month.rb
@@ -1,25 +1,42 @@
 class Month
-  attr_reader
+  attr_reader :month, :year, :expenses, :reconciled
   @@all = []
   @@months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
   def Month.add_expense(expense)
+    month = expense.date.mon
+    year = expense.date.year
     # add an expense to the corresponding Month instance
     # should be able to do this automatically based on the date
-    # if that month doesn't exist, create it
-
+    if Month.lookup(month, year) == nil
+      # if that month doesn't exist, create it
+      Month.new(month, year)
+      Month.add_expense(expense)
+    else
+      # add expense to that month
+      Month.lookup(month, year).expenses << expense
+      return Month.lookup(month, year)
+    end
   end
 
-  def initialize(index, year)
-    @@all << self
-    @month = index
+  def Month.lookup(month, year)
+    @@all.find{|instance| instance.month == month && instance.year == year}
+  end
+
+  def Month.all
+    return @@all
+  end
+
+  def initialize(month, year)
+    @month = month
     @year = year
     @expenses = []
     @reconciled = false
+    @@all << self
   end
 
   def print_name
-    p "#{@@months[@month]} #{@year}"
+    p "#{@@months[@month + 1]} #{@year}"
   end
 
 
