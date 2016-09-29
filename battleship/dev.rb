3278622e08aafe056817c456fa720b5921e934fa
Mike Flores <michaelprflores@gmail.com>
2016-09-29 12:07:46 -0400

    Rethinking the board and now creating a class of squares

--- battleship/dev.rb
+++ battleship/dev.rb
@@ -2,27 +2,36 @@
 # build the board
 # vertical and horizontal axis
 # use numbers for vertical and alphabet for horizontal
-# I can use enumberables to create a board like I did card deck
-# but I will manually create the board
+# I totally need to use an enumerable method to create my board
 
-# create the board
-# create the battleships
+# create the battleships (What, why, where, when)
+#   - they are initialized and randomly placed on the board. Part of the board class?
+#   - take up 5 spaces on the board
+#   - they can be placed horizontally or vertical
+# battleships 
 # create the player and what the player can do
 
 require 'pry'
 # using symbols cuz they faster
-board = {
-    :a => 1, 
-    :b => 2, 
-    :c => 3,
-    :d => 4,
-    :e => 5,
-    :f => 6,
-    :g => 7,
-    :h => 8,
-    :i => 9,
-    :j => 10
-}
+
+h_axis = ('a'..'j').to_a
+v_axis = (1 .. 10).to_a
+board = []
+
+h_axis.each do |h|
+    v_axis.each_with_index do |v|
+        board << [h,v]
+    end
+end
+
+puts board.join(" , ")
+
+# suits.each do |suit|
+#     ranks.each_with_index do |rank|
+#     deck << [suit, rank]
+#     deck.shuffle!
+#     end
+# end
 
 
 binding.pry
