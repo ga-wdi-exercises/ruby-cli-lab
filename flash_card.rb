9c90ce4bf35a4e0c172c454742690dfb67287127
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-09-29 11:35:58 -0400

    commit1

--- flash_card.rb
+++ flash_card.rb
@@ -1,13 +1,16 @@
 require 'pry'
+
 class Menu
   def self.display
     while 1
-      puts "Choose one of the following:"
-      puts "1 - Option 1"
-      puts "2 - Option 2"
-      puts "3 - Option 3"
+      puts "Please pick one of the following options:"
+      puts "1 - Create New Flashcard"
+      puts "2 - View my Flashcards"
+      puts "3 - Edit a Flashcard"
+      puts "4 - Delete a Flashcard"
+
       input = gets.chomp
-      if ["1","2","3"].include? input
+      if ["1","2","3","4","5"].include? input
         self.select input
         break
       else
@@ -22,9 +25,28 @@ end
 
 Menu.display
 
+class Flashcard
+  attr_accessor :front, :back
+  def initialize front, back
+    @front = front
+    @back = back
+  end
+end
+
+def card_collection(hash.txt)
+  hash_values = self.class.load_hash(hash.txt)
+  @collection.insert_many(hash)
+end
+
+def initialize
+  @collection = self.class.collection
+end
+
+Flashcard.new "Buenos Dias", "Good morning"
+
 
 
 
 
 binding.pry
-end "thanks for playing"
+puts "thanks for playing"
