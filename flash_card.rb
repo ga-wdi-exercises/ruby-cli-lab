bee3d0400f3fa84055e08c721eb67f08918797b7
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-29 14:25:07 -0400

    created while loop

--- flash_card.rb
+++ flash_card.rb
@@ -3,34 +3,57 @@
 class FlashCard
   attr_accessor :front, :back, :cards
   @@all = []
+
   def initialize front, back
     @front = front
     @back = back
-    @cards = [["Na","Sodium"],["H", "Hydrogen"]]
-    @cards << self
+    @cards = []
+    @@all.push(self)
   end
+
+  def add_card
+    @cards.push(self)
+  end
+
+  def self.all
+     @@all
+end
 end
 
+card1 = FlashCard.new("Na", "Sodium")
+card2 = FlashCard.new("H", "Hydrogen")
 
 
+while 1
 puts "enter 1 to add card"
 puts "Enter 2 to view all cards"
 puts "Enter 3 to edit card"
 puts "Enter 4 to delete card"
+puts "Enter 5 to exit"
 input = gets.chomp.to_i
 if input == 1
     p "enter a front"
     front = gets.chomp
     p "enter a back"
     back = gets.chomp
-    FlashCard.new(front,back)
-    @cards.push(FlashCard.new(front,back))
-    puts @cards
-
+    p FlashCard.new(front,back)
 
 elsif input ==2
-  p @cards
+     FlashCard.all.each do |card|
+       puts card.front + "" + card.back
+
+     end
+elsif input == 5
+  break
+
 end
+end
+# elsif input == 5
+#   FlashCard.all.each do |card|
+#     puts
+
+
+
 
 
 # binding.pry
