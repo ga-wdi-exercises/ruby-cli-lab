ff6122a3c1a309bef47bec019f2cc1b42782c60c
Mike Flores <michaelprflores@gmail.com>
2016-09-29 15:39:42 -0400

    working on Player class

--- flash_cards/dev.rb
+++ flash_cards/dev.rb
@@ -1,30 +1,70 @@
+# Write Plan and breakdown
+# need to create three classes
+# Player
+# Deck
+# Flashcard
+# The Player has a decks (collection of Decks) and in each Deck
+# there are collections are cards (collection of Cards)
+
 require 'pry'
 
+class Player
+    def initialize name
+        @name = name
+        @decks = []
+    end
+
+    def add_Deck deck_name
+        @decks.push(deck_name: deck_name)
+    end
+end
+
+# test Player
+mike = Player.new("Mike")
 
 class Flashcard
-    attr_accessor
+    attr_accessor :front, :back
     def initialize front, back
         @front = front
         @back = back 
     end
-
-    def create_new_card
+    
+    # methods to view / read
+    def read_front
+        puts @front
     end
 
-    def edit_card
+    def read_back
+        puts @back
+    end
+
+    def view
+        puts "Front: #{@front} Back: #{@back}"
+    end
+
+    def edit front
+        @front = front
+    end
+
+    def edit back
+        @back = back
+    end
+end
+
+class Deck
+    attr_accessor :cards
+    def initialize
+        cards = []
+    end
+
+    # methods to add delete cards from deck
+    def add_card name
+        name.cards.push()
     end
 
     def delete_card
     end
-
-    def view_card
-    end
-
-    def view_score
-    end
 end
-
-
 
 binding.pry
 puts "done"
