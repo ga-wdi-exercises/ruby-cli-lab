05c4730a720b857f785558490568e26526b049d8
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-29 16:04:53 -0400

    edit function

--- flash_card.rb
+++ flash_card.rb
@@ -40,17 +40,35 @@ elsif input ==2
      FlashCard.all.each do |card|
        puts (card.front + ", " + card.back).inspect
      end
-#
+elsif input == 3
+  puts "Looks like you want to edit the content. Enter the word you would like to change"
+  # if edit == "Front"
+    FlashCard.all.each do |card|
+      puts (card.front + ", " + card.back).inspect
+    end
+    word = gets.chomp
+    FlashCard.all.each do |card|
+      if word == ( card.front || card.back)
+        puts "What is the new word?"
+        new_word = gets.chomp
+        puts new_word
+        # puts "The most updated card is " + "[" + new_word + ", " + card.back + "]"
+      end
+end
+
+
 elsif input == 5
   score = 0
+  i = 0
+  j = 0
   FlashCard.all.each do |card|
-    puts "What is " + card.front + "?"
+    puts " Question " + (i+=1).to_s + ": What is " + card.front + "?"
       ans = gets.chomp
       if ans == card.back
         score += 1
-        puts "You got " + score.to_s + " !"
+        puts "Yes! You got " + score.to_s + " out of "+ (j+=1).to_s + " correct!"
       else
-      puts "wrong"
+      puts "Oops! Try again next time"
     end
 end
 #
