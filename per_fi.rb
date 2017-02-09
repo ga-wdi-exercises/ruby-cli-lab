498d9ce98bb94ba4149024a6a5081c8ec55de1a8
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-09 09:43:48 -0500

    initial commit

---
+++ per_fi.rb
@@ -0,0 +1,25 @@
+require "pry"
+
+class Enter_Transaction
+  attr_accessor :payee :input :date :category
+    @@all = []
+end
+def initialize
+  puts "What would you like to access"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  binding.pry
