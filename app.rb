72559af02a0dff97593b8aa7439ec88534d7f3a5
Dan Lin <dlin2102@gmail.com>
2017-02-09 12:26:57 -0500

    fixed part1

--- app.rb
+++ app.rb
@@ -1,48 +1,55 @@
 require "pry"
 
 
-      while 1
-        puts "Welcome to the game"
-        puts "Enter 1 to add card"
-        puts "Enter 2 to view all cards"
-        puts "Enter 3 to edit card"
-        puts "Enter 4 to delete card"
-        puts "Enter 5 to exit"
-        input = gets.chomp
+class FlashCards
+  attr_accessor :front, :back
+  @@cards = []
 
-      if input == "1"
-        puts "Enter a front card value"
-        input = gets.chomp
-        puts "Enter a back card value"
-        input = gets.chomp
-        puts Flash_Cards.new(front,back)
-      elsif input == "2"
-
-      elsif input == "3"
-
-      elsif input == "4"
-
-      elsif input == "5"
-
-      else input != ["1","2","3","4","5"]
-        break
+  def initialize(front, back)
+    @@cards.push self
+    @front = front
+    @back = back
   end
+
+  def self.all
+    @@cards
+  end
+
+
+end
+
+while 1
+  puts "Welcome to the game"
+  puts "Enter 1 to add card"
+  puts "Enter 2 to view all cards"
+  puts "Enter 3 to edit card"
+  puts "Enter 4 to delete card"
+  puts "Enter 5 to exit"
+  input = gets.chomp
+
+  if input == "1"
+    puts "Enter a front card value"
+    input_front = gets.chomp
+    puts "Enter a back card value"
+    input_back = gets.chomp
+    puts FlashCards.new(input_front,input_back)
+  elsif input == "2"
+    puts @@all
+
+  elsif input == "3"
+
+  elsif input == "4"
+
+  elsif input == "5"
+    break
+  else input != ["1","2","3","4","5"]
+    puts "Try again"
+    break
+  end
+
 end
 
 
-class Flash_Cards
-  attr_accessor :front, :back,
-  def initialize(front_index, back_index)
-    @front = front_index
-    @back = back_index
-    @cards = []
-    @score = 0
-  end
-end
 
 
-
-
-
-
-binding pry
+binding.pry
