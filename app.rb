8c88cfbb37032cc2f40a26909a8b965e0f042ef0
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-09 11:59:53 -0500

    added the CLI framework

--- app.rb
+++ app.rb
@@ -1,117 +1,67 @@
 #Personal Finance
 
 require "pry"
+require_relative "methods"
 
-#Global Variables
-$transactions = []
-$balance = 0
-
-#Building the Class
-class Transaction
-  attr_accessor :payee, :amount, :date, :category
-  def initialize(payee, amount, date, category)
-    @payee = payee
-    @amount = amount
-    @date = date
-    @category = category
-  end
-end
-
-#Add Transaction
-def add_transaction(payee, amount, date, category)
-  $transactions << Transaction.new(payee, amount, date, category)
-  $balance += amount.to_i
-end
-add_transaction("John", 40, "1/12/2017", "food")
-add_transaction("Mike", -20, "1/13/2017", "transportation")
-add_transaction("Mark", 200, "1/14/2017", "fun")
-add_transaction("Jane", -10, "1/15/2017", "food")
-
-#List Transactions
-def list_transactions
-  p $transactions
-end
-
-#Edit Transaction
-def edit_transaction
-  puts "Here are the existing transactions:"
-  p $transactions
-  puts "What is the index number of the transaction you want to edit?"
-  user_input = gets.chomp.to_i
-  puts "You chose this transaction:"
-  p $transactions[user_input]
-  puts "Which part of the transaction do you want to edit?"
-  puts "To edit the payee type 1!"
-  puts "To edit the amount type 2!"
-  puts "To edit the date type 3!"
-  puts "To edit the category type 4!"
-  user_input2 = gets.chomp.to_i
-  if user_input2 == 1
-    puts "What is the new name of the payee for this transaction?"
-    user_input3 = gets.chomp.to_s
-    $transactions[user_input].payee = user_input3
-    puts "Your transaction was updated. Here are your updated transactions:"
+#Main CLI frameword
+puts "Welcome to Mike Finances, the best way to manage your personal finances."
+def start
+  puts "How can I do for you?"
+  puts "- To review a list of your current transactions type 1"
+  puts "- To add a new transaction type 2"
+  puts "- To delete a transaction type 3"
+  puts "- To edit an existing transaction type 4"
+  puts "- To display current balance type 5"
+  puts "- To list transactions by category type 6"
+  user_input_main = gets.chomp.to_i
+  if user_input_main == 1
+    list_transactions
+    start
+  elsif user_input_main == 2
+    puts "Who is the payee of this new transaction?"
+    user_input_a = gets.chomp.to_s
+    payee = user_input_a
+    puts "What is the amount of this new transaction?"
+    user_input_b = gets.chomp.to_s
+    amount = user_input_b
+    puts "what is the date of this new transaction?"
+    user_input_c = gets.chomp.to_s
+    date = user_input_c
+    puts "Which category does this new transaction belong to?"
+    user_input_d = gets.chomp.to_s
+    category = user_input_d
+    add_transaction(payee, amount, date, category)
+    puts "Here is the updated list of your transactions:"
     p $transactions
-  elsif user_input2 == 2
-    puts "What is the new amount of this transaction?"
-    user_input4 = gets.chomp.to_i
-    $balance -= $transactions[user_input].amount
-    $transactions[user_input].amount = user_input4
-    $balance += user_input4
-    puts "Your transaction was updated. Here are your updated transactions:"
+    start
+  elsif user_input_main == 3
+    puts "Here is the list of your existing transactions:"
     p $transactions
-    puts "Your current balance is:"
-    p $balance
-  elsif user_input2 == 3
-    puts "What is the new date of this transaction?"
-    user_input5 = gets.chomp.to_s
-    $transactions[user_input].date = user_input5
-    puts "Your transaction was updated. Here are your updated transactions:"
+    puts "Please type the index number of the transaction which you want to delete!"
+    user_input_e = gets.chomp.to_i
+    index = user_input_e
+    delete_transaction(index)
+    puts "Here is the updated list of your transactions:"
     p $transactions
-  elsif user_input2 == 4
-    puts "What is the new category this transaction?"
-    user_input6 = gets.chomp.to_s
-    $transactions[user_input].category = user_input6
-    puts "Your transaction was updated. Here are your updated transactions:"
-    p $transactions
-  else
-    puts "Sorry! I didn't understand this. Please try again!"
+    start
+  elsif user_input_main == 4
     edit_transaction
+    start
+  elsif user_input_main == 5
+    display_balance
+    start
+  elsif user_input_main == 6
+    puts "Which category do you want to select?"
+    user_input_f = gets.chomp.to_s
+    num = user_input_f
+    list_by_category(num)
+    start
+  else user_input_main == 7
+    puts "Sorry, I didn't undersatnd this. You have to put a number between 1 and 6. Please try again!"
+    start
   end
 end
-
-#Delete Transaction
-def delete_transaction(index)
-  $transactions.delete_at(index)
-  p $transactions
-end
-
-#Display current balance
-def display_balance
-  p $balance
-end
-
-#List Transactions by category
-def list_by_category(num)
-  by_category = []
-  for transaction in $transactions
-    if transaction.category == num
-      by_category << transaction
-    else
-      next
-    end
-  end
-  puts "Here are your transactions in category #{num}"
-  p by_category
-  category_cost = 0
-  puts "Here is the cost of transactions in this category:"
-  for ruby_transaction in by_category
-    category_cost += ruby_transaction.amount
-  end
-  p category_cost
-end
-list_by_category("food")
-
+start
 
 # binding.pry
 
