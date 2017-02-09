300a30095475d3b5f50ddac9bda026749cec255c
Lisa Anne Burr <burr_lisa@bah.com>
2017-02-09 10:33:33 -0500

    Added classes to make a card, cards, decks, and games

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require "pry"
 
+class Menu
+
+
+end
+
+
 class Player
   attr_accessor :name, :favorite_food
 
@@ -13,28 +19,50 @@ class Player
   end
 end
 
+class Games
+  attr_accessor :games
 
+  def initialize(games = [])
+    @games = games
+  end
 
-class Game
-
+  def add_card_deck(card_deck)
+    @card_deck << card_deck
+  end
 end
 
 
+class Deck
+  attr_accessor :card_deck
 
-class Menu
+  def initialize(card_deck = [])
+    @card_deck = card_deck
+  end
 
-
-
+  def add_card(card)
+    @card << card
+  end
 end
-
-
 
 class Cards
-  attr_accessor :card_front, :card_back
+  attr_accessor :card
 
-  def initialize (card_front, card_back)
+  def initialize(card = [])
+    @card = card
+  end
+
+  def add_question(question)
+    @question << question
+  end  
+end
+
+class Question
+  attr_accessor :card_front, :card_back, :category
+
+  def initialize (card_front, card_back, catergory)
     @card_front = card_front
     @card_back = card_back
+    @category = category
   end
 end
 
@@ -44,5 +72,13 @@ end
 
 
 lisa = Player.new("Lisa", "chips and dip")
+math1 = Question.new("1 + 1 = ", "2", "Maths")
+james = Player.new("James", "Skittles")
+andy = Player.new("Andy", "pistachios")
+alice = Player.new("Alice", "PB&J")
+kanye = Player.new("Kanye", "sushi")
+gaga = Player.new("Lady Gaga", "Reeses Pieces")
+minnesota = Question.new("What is the capital of Minnesota?", "St. Paul", "State Capitals")
+
 
 binding.pry
