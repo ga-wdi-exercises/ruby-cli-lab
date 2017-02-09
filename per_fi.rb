698eeea602a86904a5f2fe3c746d5bd418cb71fa
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-09 12:08:38 -0500

    working on trans history and balance, both not working

--- per_fi.rb
+++ per_fi.rb
@@ -5,21 +5,37 @@ class Enter_Transaction
     @@all = []
 
     def initialize
-      puts "Enter a payee."
+      puts "First, enter a payee."
         @payee = gets.chomp
-      puts "Enter an amount."
+      puts "Second, enter an amount."
         @amount = gets.chomp
-      puts "Enter the date."
+      puts "Third, enter today's date."
         @date = gets.chomp
-      puts "Enter your category."
+      puts "Lastly, enter your category."
         @category = gets.chomp
       @@all << self
       end
+
+      def self.view
+        return @@all
+
+      end
   end
+
+class Transaction_History < Enter_Transaction
+  def self.history
+      super (@@all)
+    end
+  end
+class Current_Balance < Enter_Transaction
+  def self.Balance
+    @amount += input
+  end
+end
 
   class Menu
     def self.display
-      while 1
+      while input 1 || 2 || 3
         puts "Choose one of the following:"
         puts "1 - New Transaction"
         puts "2 - Transaction History"
@@ -37,12 +53,16 @@ class Enter_Transaction
       puts "You selected #{number}"
       if number == 1
         puts "Enter Transaction"
+        Enter_Transaction.new
       end
       if number == 2
         puts "Your Recent Activies"
+        Transaction_History.new
       end
       if number == 3
         puts "Current Balance"
+        Current_Balance.display
+      end
     end
   end
 
