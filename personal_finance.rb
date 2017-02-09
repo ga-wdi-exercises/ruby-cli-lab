2b50c8ea4de1beeb7e9503d5ed1cd776809ff007
Elena Santana <fanelenan7@gmail.com>
2017-02-09 12:29:29 -0500

    did all I could, input is not being recognized for some reason.

--- personal_finance.rb
+++ personal_finance.rb
@@ -1,7 +1,3 @@
-# require "pry"
-# puts "Please enter the name of payee, amount and category."
-
-
 class Transaction
   attr_accessor :name, :amount, :date, :category, :transactions
 
@@ -13,60 +9,63 @@ class Transaction
   end
 
   finance_on = true
-  # while finance_on == true
-
   while finance_on == true
-  def begin
-      puts "Welcome to Personal Finance. How can we assist you today?"
-      puts "1. Desposit"
-      puts "2. Withdraw"
-      puts "3. View Transaction History"
-      puts "4. Edit Transactions."
-      puts "5. Delete Transactions."
-      puts "6. View Current Balance."
-      input = gets.chomp
-      if input == "1"
-        self.deposit
-      elsif input == "2"
-        self.withdraw
-      elsif input == "3"
-        self.transaction_history
-      elsif input == "4"
 
-      elsif input == "5"
-        self.delete_transaction
-      elsif input == "6"
-
-      else
-        puts "Invalid option."
-      end
+    puts "Welcome to Personal Finance. How can we assist you today?"
+    puts "1. Desposit"
+    puts "2. Withdraw"
+    puts "3. View Transaction History"
+    puts "4. Edit Transactions."
+    puts "5. Delete Transactions."
+    puts "6. View Current Balance."
+    # Doesn't recognize the method being called for some reason. Tried moving loop around, tried self. and without self. in both method name and conditional ¯\_(ツ)_/¯ smaller methods are working though, yay? Did not get to "edit" while trying to figure out why the thing is not starting.
+  # def begin
+    input = gets.chomp
+    if input == "1"
+      self.deposit
+    elsif input == "2"
+      self.withdraw
+    elsif input == "3"
+      self.transaction_history
+    elsif input == "4"
+      # did not get to edit
+    elsif input == "5"
+      self.delete_transaction
+    elsif input == "6"
+      puts @amount
+    else
+      puts "Invalid option."
     end
     finance_on == false
+  # end
 
-    def deposit
-      puts "How much money would you like to deposit?"
-      new_input = gets.chomp.to_i
-      @amount = @amount + new_input
-      puts {"Your balance is now $#{@amount}."}
-      transactions << self
-    end
+  def self.deposit
+    puts "How much money would you like to deposit?"
+    new_input = gets.chomp.to_i
+    @amount = @amount + new_input
+    puts {"Your balance is now $#{@amount}."}
+    transactions << self
+  end
 
-    def withdraw
-      puts "How much money would you like to withdraw?"
-      new_input = gets.chomp.to_i
-      @amount = @amount - new_input
-      puts {"Your balance is now $#{@amount}."}
-      transactions << {transaction: self, date: @date}
-    end
+  def self.withdraw
+    puts "How much money would you like to withdraw?"
+    new_input = gets.chomp.to_i
+    @amount = @amount - new_input
+    puts {"Your balance is now $#{@amount}."}
+    transactions << {transaction: self, date: @date}
+  end
 
-    def transaction_history
-      puts "You have total of #{transactions.length} transactions."
-      puts transactions
-    end
+  def self.transaction_history
+    puts "You have total of #{transactions.length} transactions."
+    puts transactions
+  end
 
-    def delete_transaction
-
-
+  def self.delete_transaction
+    puts "Which transaction would you like to delete? Please input the number."
+    puts transactions
+    input = gets.chomp.to_i
+    transactions.delete_at(input+1)
+    puts "Transaction deleted."
   end
 
 end
@@ -74,6 +73,8 @@ end
 # end
 # Transaction.new("John Smith", "100", "Debit")
 # binding.pry
+
+# gutted original menu
 
 #   puts "Welcome to Personal Finance. How can we assist you today?"
 #   options = [
