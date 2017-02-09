fe84966f38fb331a15faccd8bab8d0d69e14b99f
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-09 10:03:48 -0500

    fixed board, added prompt user

--- app.rb
+++ app.rb
@@ -1,7 +1,36 @@
+require "pry"
+
+# Flashcard board
+cardlist = []
+card = []
+cardlist.product(card)
+
 class Flashcards
+  attr_accessor :front, :back
+
   def initialize(front, back)
     @front = front
     @back = back
   end
 
+# prompt user to create cards
+  def card
+    index = 0
+    loop do
+      puts "Welcome, create a #{card}"
+    cardlist  << {front:front, back:back}
+    index +=1
+  end
+  end
+
 end
+
+
+
+
+
+
+
+
+
+binding.pry
