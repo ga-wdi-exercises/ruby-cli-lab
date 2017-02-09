da55f185e12a1361d1b97f1b0eb98ff1c9588747
Andy Flickinger <atflick@gmail.com>
2017-02-09 11:32:58 -0500

    gameboard and menu built

---
+++ battleship.rb
@@ -0,0 +1,93 @@
+
+$line_break = "#" * 55
+
+class GameBoard
+  attr_accessor :grid, :cell
+  def initialize
+    @grid = []
+    @cell = {display: "~~", ship: false}
+    self.build_grid
+  end
+
+  def build_grid
+    row = []
+    10.times {row << self.cell}
+    10.times {@grid << row}
+  end
+  def print_head
+    system "clear"
+    puts $line_break
+    puts "##" + " "*51 + "##"
+    puts "##" +" "*20+"BATTLESHIP "+" "*20+"##"
+    puts "##" + " "*51 + "##"
+    puts $line_break
+  end
+  def print_grid
+    self.print_head
+    puts $line_break
+    i = 0
+    grid.each do |row|
+      row_label = ["A","B","C","D","E","F","G","H","I","J"]
+      printable_row = []
+      row.each do |cell|
+        printable_row << cell[:display]
+      end
+      row_string = printable_row.join(" | ")
+      puts "## #{row_label[i]} #{row_string} ##"
+      # puts "##   #{row_string} ##"
+      puts "##" + "-" * 51  + "##"
+      i += 1
+    end
+    bottom_row = []
+    l = 0
+    10.times do
+      bottom_row << " #{l}  "
+      l += 1
+    end
+    print_row = bottom_row.join("|")
+    puts "##  #{print_row}##"
+    puts $line_break
+  end
+
+end
+
+class MenuPrompts
+  attr_accessor :scores, :tries, :input
+  def initialize
+    @score = 0
+    @tries = 10
+    @input = ""
+  end
+
+  def welcome
+    system "clear"
+    puts $line_break
+    puts "##" + " "*51 + "##"
+    puts "##" +" "*14+"Welcome to Battleship!!"+" "*14+"##"
+    puts "##" + " "*51 + "##"
+    puts $line_break
+    puts "##" + " "*51 + "##"
+    puts "##" + " "*16+"1. Play new game   "+" "*16+"##"
+    puts "##" + " "*16+"2. Resume game     "+" "*16+"##"
+    puts "##" + " "*16+"3. Quit            "+" "*16+"##"
+    puts "##" + " "*51 + "##"
+    puts $line_break
+    # self.input = ""
+    while self.input != "quit"
+      self.input = gets.chomp.downcase
+      if self.input == "1"
+        $new_game.print_grid
+      elsif self.input == "2"
+
+      else
+        puts "Invalid response"
+      end
+    end
+  end
+
+end
+
+$new_game = GameBoard.new
+$prompt = MenuPrompts.new
+
+$prompt.welcome
