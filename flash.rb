71683e6d401456d65486eb8c2c2c8fd1498280c0
Natasha Clark <Natasharclark@gmail.com>
2017-02-09 12:11:47 -0500

    trying to create new cards method

--- flash.rb
+++ flash.rb
@@ -1,104 +1,65 @@
 require "pry"
 
 class Menu
-  puts "Welcome to FlashCards!"
-  puts "Please indicated what you would like to do:"
-  puts "Create Cards -- 1"
-  puts "View Cards-- 2"
-  puts "Edit Cards --3"
-  puts "Play FlashCards --4"
-
-  answer = gets.chomp
-    if answer == "1"
-      Card.display
-    elsif answer == "2"
-      # open class cards
-      puts "view"
-    elsif answer == "3"
-        # open class cards
-        puts "edit"
-    elsif answer == "4"
-        # open class cards
-        puts "play "
-    else
-      puts "sorry, that is an invalid resopnse "
+  def self.display
+    while 1 || 2 || 3 || 4
+      puts "Welcome to FlashCards!"
+      puts "Choose one of the following:"
+      puts "1 - Create Cards 1"
+      puts "2 - View Cards 2"
+      puts "3 - Edit Cards 3"
+      puts "4 - Play FlashCards 4"
+      input = gets.chomp
+      if ["1","2","3", "4"].include? input
+        self.select input
+        break
+      else
+        puts "Invalid option."
+      end
     end
+  end
+  def self.select number
+    puts "You selected #{number}"
+    if number == 1
+      Class.new
+    elsif number == 2
+      puts "2"
+    elsif number == 3
+      puts "3"
+    elsif number == 4
+      puts "4"
+  end
 end
-# Menu.display
+end
 
-class Card < Menu
+Menu.display
+
+class Card
 
 puts "You have selcted to create new flash cards. To create new cards, enter your questions to be displayed  on the front of the card and your answers to be displayed on the back. You will also want to set a catagory for each card as well "
 
-  attr_accessor :category, :front, :back
-  front = []
-  back = []
-  catagory = []
+  attr_accessor :category, :question, :answer
+  @@deck = []
 
-  def initialize
-    @question= front
-    @category = category
-    @answer = back
+  def initialize (question, answer, catagory)
+    @question= question
+    @category = catagory
+    @answer = answer
     @@deck.push(self)
   end
-
-  def set_catagory (catagory)
-    puts "Please enter catagory of card to be created"
-    cat = gets.chomp
-    category << cat
-  end
-
-  def front (front)
-    puts " Please enter the question to be displayed on the front of the flshcard"
-    question = gets.chomp
-    front << question
-  end
-
-  def back (back)
-    puts "Please enter the answer to be displayed on the back of the flashcard"
-    answer = gets.chomp
-    back << answer
-  end
-
-    def create_card
-      set_catagory
-      front
-      back
-    end
 
   def self.get_deck
     return @@deck
   end
+
+  def create_card (question, answer, catagory)
+    Card.new
+  end
+
+
 end
 
 
-# class Menu
-#   def self.display
-#     while 1
-#       puts "Choose one of the following:"
-#       puts "1 - Option 1"
-#       puts "2 - Option 2"
-#       puts "3 - Option 3"
-#       input = gets.chomp
-#       if ["1","2","3"].include? input
-#         self.select input
-#         break
-#       else
-#         puts "Invalid option."
-#       end
-#     end
-#   end
-#   def self.select number
-#     puts "You selected #{number}"
-#   end
-# end
-#
-# Menu.display
-
-
-
-
-
-
-
 binding.pry
+
+# Question to ask Adrian: How do you
