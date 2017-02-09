cfa774b71cf26a2790f3a742d1e248a21f68bfd4
Vince Campanale <vcampanale0294@gmail.com>
2017-02-09 12:26:59 -0500

    added condition so that both ships must be sunk for game to end

--- battleship.rb
+++ battleship.rb
@@ -1,25 +1,21 @@
 require_relative "ship.rb"
 require_relative "grid.rb"
 
-
 puts "Battleship!"
-puts "Sink 'em"
 puts ' '
+
 
 ship_one = Ship.new
 ship_one.build_ship
 ship_two = Ship.new
 ship_two.build_ship
 
-p ship_two.coordinates
-p ship_one.coordinates
-
 grid = Grid.new
 grid.print_grid
 grid.add_ship_one ship_one
 grid.add_ship_two ship_two
 
-until grid.ship_is_sunk? ship_one #until ship is sunk returns true for ship one
+until (grid.ship_is_sunk? ship_one) && (grid.ship_is_sunk? ship_two) #until ship is sunk returns true for ship one
   puts ' ' # add a new line to make it easier to read
   puts "Take a guess - Format: y, x.
   Note: Y increases down, X increases left to right." #promt user for a guess
@@ -46,9 +42,7 @@ until grid.ship_is_sunk? ship_one #until ship is sunk returns true for ship one
 
 end
 
-
-
-grid.ship_is_sunk? ship_one
+puts "Congratulations! You sunk both battleships!"
 
 # puts ' '
 # puts "Where would you like to place a torpedo? Letter-number format. Example: A10 or G5"
