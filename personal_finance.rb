f02d3eeccfc3e763c738067ee12b30f5bcbf3945
Elena Santana <fanelenan7@gmail.com>
2017-02-09 11:36:12 -0500

    made some progress, but can't boot the thing up

--- personal_finance.rb
+++ personal_finance.rb
@@ -1,41 +1,45 @@
 # require "pry"
 puts "Please enter the name of payee, amount and category."
 
-class Transaction
-  attr_accessor :payee, :amount, :date, :category, :transactions
 
-  def initialize(payee, amount, category)
-    @payee = payee
+class Transaction
+  attr_accessor :name, :amount, :date, :category, :transactions
+
+  def initialize
+    @name = name
     @amount = 0
-    @@date = Time.now.strftime("%d/%m/%Y %H:%M")
-    @category = nil
+    @date = Time.now.strftime("%d/%m/%Y %H:%M")
     @transactions = []
   end
-
-  def self.add_transaction(payee, amount, category)
-    puts "Please enter the name of payee, amount and category."
-    @transactions << {payee: payee, amount: amount, date: date, category: category}
-  end
+  finance_on = true
+  while finance_on = true
 
   def begin
-    puts "Welcome to Personal Finance. How can we assist you today?"
-    options = [{selection: "1. New Transaction."}, {selection: "2. View All Transactions."}, {selection: "3. Edit Transactions."}, {selection: "4. Delete Transactions."}, {selection: "5. View Current Balance."}, {selection: "6. View Transactions By Category."}]
-
-    # finance_on = true
-    # while finance_on = true
-
-    options.each {|option| puts option[:selection]}
-    puts "Enter the option number."
+    while true
+      puts "Welcome to Personal Finance. How can we assist you today?"
+      options = [
+        {selection: "1. Deposit."},
+        {selection: "2. Withdraw"},
+        {selection: "3. View All Transactions."},
+        {selection: "4. Edit/ Transactions."},
+        {selection: "5. Delete Transactions."},
+        {selection: "6. View Current Balance."},
+        {selection: "7. View Transactions By Category."}]
+      options.each {|option| puts option[:selection]}
+      puts "Enter the option number."
+    break
     input = gets.chomp
 
     if input == "1"
-      Transaction.add_transaction
+      self.deposit
     elsif input == "2"
+      self.withdraw
+    elsif input == "3"
       if transactions == []
         puts "No transaction history."
       else
+        puts transactions
       end
-    elsif input == "3"
 
     elsif input == "4"
 
@@ -43,11 +47,29 @@ class Transaction
 
     elsif input == "6"
 
+    elsif input == "7"
+
     else
       puts "Invalid option."
     end
+
+    def deposit
+      puts "How much money would you like to deposit?"
+      new_input = gets.chomp.to_i
+      @amount = @amount + new_input
+      puts {"Your balance is #{@amount}."}
+    end
+
+    def withdraw
+      puts "How much money would you like to withdraw?"
+      new_input = gets.chomp.to_i
+      @amount = @amount + new_input
+      puts {"Your balance is #{@amount}."}
+    end
+
   end
-# end
+
+  break if input == "exit"
 end
 
 # Transaction.new("John Smith", "$100", "Debit")
