c533514f2d2440be111cb333ed850ab175b467bf
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-02-09 12:26:16 -0500

    first commit

---
+++ flashcards.rb
@@ -0,0 +1,31 @@
+require "pry"
+
+
+puts '
+   ________           __    ______               __
+  / ____/ /___ ______/ /_  / ____/___ __________/ /____
+ / /_  / / __ `/ ___/ __ \/ /   / __ `/ ___/ __  / ___/
+/ __/ / / /_/ (__  ) / / / /___/ /_/ / /  / /_/ (__  )
+/_/   /_/\__,_/____/_/ /_/\____/\__,_/_/   \__,_/____/
+'
+puts '__________________'
+puts 'Spanish Flashcards'
+puts '__________________'
+
+class Flashcards
+  attr_accessor :front, :back
+  def initialize(front, back)
+    @front = front
+    @back = back
+  end
+end
+
+card []
+
+while
+
+
+
+
+
+  
