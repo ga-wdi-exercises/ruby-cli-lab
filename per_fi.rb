d0854358db972b768b429018bbe12dec8564495a
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-09 10:41:37 -0500

    added options to menu

--- per_fi.rb
+++ per_fi.rb
@@ -1,14 +1,14 @@
 require "pry"
 
 class Enter_Transaction
-  attr_accessor :payee, :input, :date, :category
+  attr_accessor :payee, :amount, :date, :category
     @@all = []
 
     def initialize
       puts "Enter a payee."
         @payee = gets.chomp
       puts "Enter an amount."
-        @input = gets.chomp
+        @amount = gets.chomp
       puts "Enter the date."
         @date = gets.chomp
       puts "Enter your category."
@@ -24,8 +24,8 @@ class Enter_Transaction
         puts "1 - New Transaction"
         puts "2 - Transaction History"
         puts "3 - Current Balance"
-        input = gets.chomp
-        if ["1","2","3"].include? input
+        input = gets.chomp.to_i
+        if [1,2,3].include? input
           self.select input
           break
         else
@@ -35,6 +35,14 @@ class Enter_Transaction
     end
     def self.select number
       puts "You selected #{number}"
+      if number == 1
+        puts "Enter Transaction"
+      end
+      if number == 2
+        puts "Your Recent Activies"
+      end
+      if number == 3
+        puts "Current Balance"
     end
   end
 
