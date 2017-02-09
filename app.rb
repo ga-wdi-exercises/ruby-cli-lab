fe0d50468d7a0913a3005e3ac4c92a2a8086e3e5
Erica Norby <ericanorby@gmail.com>
2017-02-09 11:11:57 -0500

    added budget and filter methods

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require "pry"
 
 class UserAccount
-  attr_accessor :username, :name, :transactions
-  def initialize(username, name)
+  attr_accessor :username, :name, :transactions, :balance
+  def initialize(username, name, budget)
     @username = username
     @name = name
     @transactions = []
+    @balance = budget
   end
   def add_transaction(payee, amount, date, category)
     new_transaction = {payee: payee,
@@ -13,12 +14,12 @@ class UserAccount
             date: date,
             category: category}
     @transactions << new_transaction
+    @balance -= amount
   end
 end
 
 
-erica = UserAccount.new("norbsie", "Erica Norby")
-erica.add_transaction("McDonalds", 10, "2/9/17", "Checking")
+erica = UserAccount.new("norbsie", "Erica Norby", 300)
 
 
 puts "Hello, Erica!"
@@ -26,12 +27,14 @@ puts "What would you like to do?"
 puts "**************************"
 puts "1: Add a transaction
 2: View transaction history
+3: Filter transaction history by category
+4: View balance
 0: Exit program"
 
 option = nil
 
 while option != 0
-  
+
   option = gets.chomp.to_i
 
   if option == 1
@@ -48,6 +51,17 @@ while option != 0
 
   if option == 2
     puts erica.transactions
+  end
+
+  if option == 3
+    puts "Enter a category"
+    find_category = gets.chomp
+    filtered = erica.transactions.select { |transaction| transaction[:category] == find_category }
+    puts filtered
+  end
+
+  if option == 4
+    puts "$#{erica.balance}"
   end
 end
 
