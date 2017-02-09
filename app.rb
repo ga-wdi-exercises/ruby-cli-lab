cc3fc08d402166da08f87946db68ad8b5cb9d902
Will Hawkins <willhawkins123@yahoo.com>
2017-02-09 10:06:09 -0500

    Can target better, using letters and numbers instead of index

--- app.rb
+++ app.rb
@@ -50,20 +50,19 @@ while ships_active > 0
   target = gets.chomp
   if target == "Exit" || target == "Quit" || target == "exit" || target == "quit"
     break
-  else
-    target = target.to_i
-    puts target
   end
-  if target < 101 && target > 0
-    ocean[target - 1].hit
 
-    placing_index = 0
-    row_index = 0
-    puts "    1 2 3 4 5 6 7 8 9 10\n\n"
-    10.times do |something|
-    puts "#{rows[row_index]}   #{ocean[placing_index + 0].symbol} #{ocean[placing_index + 1].symbol} #{ocean[placing_index + 2].symbol} #{ocean[placing_index + 3].symbol} #{ocean[placing_index + 4].symbol} #{ocean[placing_index + 5].symbol} #{ocean[placing_index + 6].symbol} #{ocean[placing_index + 7].symbol} #{ocean[placing_index + 8].symbol} #{ocean[placing_index + 9].symbol}"
-    placing_index += 10
-    row_index += 1
+  ocean.each do |tile|
+    if tile.id == target || tile.id == target.capitalize
+      tile.hit
     end
+  end
+  placing_index = 0
+  row_index = 0
+  puts "    1 2 3 4 5 6 7 8 9 10\n\n"
+  10.times do |something|
+  puts "#{rows[row_index]}   #{ocean[placing_index + 0].symbol} #{ocean[placing_index + 1].symbol} #{ocean[placing_index + 2].symbol} #{ocean[placing_index + 3].symbol} #{ocean[placing_index + 4].symbol} #{ocean[placing_index + 5].symbol} #{ocean[placing_index + 6].symbol} #{ocean[placing_index + 7].symbol} #{ocean[placing_index + 8].symbol} #{ocean[placing_index + 9].symbol}"
+  placing_index += 10
+  row_index += 1
   end
 end
