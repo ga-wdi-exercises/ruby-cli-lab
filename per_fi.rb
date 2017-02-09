03c2a7809389ef65219445bc4ae886936aa4f5e7
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-09 10:11:28 -0500

    made menu

--- per_fi.rb
+++ per_fi.rb
@@ -15,11 +15,30 @@ class Enter_Transaction
         @category = gets.chomp
       @@all << self
       end
-end
+  end
 
-# class Menu
+  class Menu
+    def self.display
+      while 1
+        puts "Choose one of the following:"
+        puts "1 - New Transaction"
+        puts "2 - Transaction History"
+        puts "3 - Current Balance"
+        input = gets.chomp
+        if ["1","2","3"].include? input
+          self.select input
+          break
+        else
+          puts "Invalid option."
+        end
+      end
+    end
+    def self.select number
+      puts "You selected #{number}"
+    end
+  end
 
-
+  Menu.display
 
 
 
