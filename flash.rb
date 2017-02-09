be191c7385a6b25c773e1f62b7edbfd1a96924cf
Ebenezer Williams <ebenezerw@me.com>
2017-02-09 10:07:18 -0500

    initial commit

---
+++ flash.rb
@@ -0,0 +1,48 @@
+require "pry"
+
+puts "Welcome to Flashcard. Please type enter to continute"
+
+class Flashcard
+  attr_accessor :front, :back
+  def initialize (front, back)
+    @front = front
+    @back = back
+  end
+end
+
+spanish = Flashcard.new("Buenos Dias", "Good morning")
+italian =
+french =
+russian =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+binding.pry
