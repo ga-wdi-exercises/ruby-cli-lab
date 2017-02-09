3299f9c6ddb42b431a5f3bc957d2166d203fe5e0
Samuel Clements <sdkclements@gmail.com>
2017-02-09 12:17:00 -0500

    getting game logic for finding cords

--- battleship/app.rb
+++ battleship/app.rb
@@ -3,23 +3,45 @@ require_relative 'models/game_board'
 require_relative 'models/torpedo'
 
 # CREATE BOARD
-game_board = initialize_board(4, '(empty)')
+game_board = initialize_board(10, "(~)")
 
 # Create Boats
-row_boat = Battleship.new(5)
+row_boat = Battleship.new(2)
+
+p '-----'
 
 #Place Boats
+#  uncomment below for later for now we will use a diff starting pos
+# starting_position = Random.new.rand(1..(5 * 5))
+ starting_position = 4
+
+
+ # def place_ship(num)
+ # 	starting_position = 4
+
+ 	
+ # end
+
+ p game_board.length
+
+
+# --------------------
+# Print Pretty Board
+ # p show_board(game_board)
+
+
 
 # Start Game
+
 
 #locate Position
 # def find_start_position
 
 #showboat
 p row_boat.show_boat
-
-p Random.new.rand(0..(10 * 10))
+p '-----'
 
 p game_board
+
 
 
--- battleship/models/game_board.rb
+++ battleship/models/game_board.rb
@@ -5,12 +5,23 @@ def initialize_board(n, space)
 		n.times {|col_piece| row << space }
 		board << row
 	end
+	board
+	
+end
+
+
+
+def show_board(board)
 	board.map! do |arr| 
 		arr.join + "\n"
 	end
 	board.each {|row| puts row}
+
 end
 
+
+# boardst = initialize_board(5, '(~)')
+# show_board(boardst)
 # def show_board(board)
 # 	board.map! do |arr| 
 # 		arr.join + "\n"
@@ -19,10 +30,10 @@ end
 # end
 
 
-board = initialize_board(6, "(~)")
-p '-----'
-board_arr = []
+# board = initialize_board(6, "(~)")
+# p '-----'
+
 # board_arr << board
 # puts board_arr
-puts board
+# puts board
 
