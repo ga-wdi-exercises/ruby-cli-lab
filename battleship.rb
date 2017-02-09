90b35bb366326106d5c93d467330812c20eeab92
Vince Campanale <vcampanale0294@gmail.com>
2017-02-09 12:23:36 -0500

    game works with two ships - however overlapping is not prevented

--- battleship.rb
+++ battleship.rb
@@ -11,6 +11,9 @@ ship_one.build_ship
 ship_two = Ship.new
 ship_two.build_ship
 
+p ship_two.coordinates
+p ship_one.coordinates
+
 grid = Grid.new
 grid.print_grid
 grid.add_ship_one ship_one
@@ -18,16 +21,29 @@ grid.add_ship_two ship_two
 
 until grid.ship_is_sunk? ship_one #until ship is sunk returns true for ship one
   puts ' ' # add a new line to make it easier to read
-  puts "Take a guess - format: x, y" #promt user for a guess
+  puts "Take a guess - Format: y, x.
+  Note: Y increases down, X increases left to right." #promt user for a guess
+
   guess = gets.chomp #chomp the guess
 
   if ship_one.is_hit? guess #check if the guess is a hit
     grid.add_hit guess #if it is, add an H in that position
+    puts "#{guess} is a hit!"
+  else
+    grid.add_miss guess #if it's not, add an M
+  end
+
+  if ship_two.is_hit? guess #check if the guess is a hit
+    grid.add_hit guess #if it is, add an H in that position
+    puts ' '
+    puts "#{guess} is a hit!"
+    puts ' '
   else
     grid.add_miss guess #if it's not, add an M
   end
 
   grid.print_grid #reprint the grid
+
 end
 
 
