dd4cbc06544ee1cb01de2c4ebd3246d727c0e003
meagdoh <meagdoherty@gmail.com>
2017-04-12 13:27:44 -0400

    Switch from flash cards to transactions

--- app.rb
+++ app.rb
@@ -1,39 +1,54 @@
 require 'pry'
 
-#  Prompt user to
-  # 1 ==> Create a new flashcard
-  # 2 ==> View all flashcards
-  # 3 ==> Edit a flashcard
-  # 4 ==> Delete a flashcard
-  # 5 ==> View score / recent answers
+Users should be able to enter their transactions (which have a payee, an amount, a date, and a category).
+They can see a list of all transactions, edit / delete transactions, and see their current balance.
+Users should be able to look at transactions by category (i.e. filtering), and see the total 'cost' of transactions in that category.
 
+class Transaction
+attr_accessor :payee, :amount, :date, :category
 
-# Create a new flashcard
-puts "Create a new flash card!"
-gets #{front} and #{back}
+  def initialize
+    @payee = payee
+    @amount = amount
+    @date = date
+    @category = category
 
+class Account
+attr_accessor :transactions, :balance
 
+  def initialize
+    @transactions = []
+    @balance = 0
+  end
 
-# View all flashcards
-# Edit a flashcard
-# Delete a flashcard
-# View score / recent answers
-#
-#
-# The player should be able to see their score and how many cards/questions they got right.
-# Allow users to create lists or categories of flashcards.
-# Allow users to view only the flashcards in one particular category or another.
+  def show_options
+      puts 'enter option'
+      puts '1 - enter new transaction'
+      puts '2 - view previous transactions'
+      puts 'exit - exit program'
+      input = gets.chomp
+      if(input == '1')
+        enter_new_transaction
+      end
+  end
 
-
-
-class Flashcard
-  attr_accessor :front, :back
-  def initialize front, back
-    @front = front
-    @back = back
+  def enter_new_transaction
+    puts 'enter transaction amount'
+    amount = gets.chomp
+    puts 'enter transaction category'
+    category = gets.chomp
+    puts 'credit or debit (c/d)'
+    type = gets.chomp == 'c' ? 'credit' : 'debit'
+    @transactions.push(Transaction.new(amount, category, type))
   end
 end
 
-Flashcard.new "Buenos Dias", "Good morning"
+
+account = Account.new
+
+loop do
+  account.show_options
+end
+
 
 binding 'pry'
