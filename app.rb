e42234ca85c40603b12ad5a636402edceff47bdb
Whitney <whitney.purdum@gmail.com>
2017-04-12 16:31:47 -0400

    update

--- app.rb
+++ app.rb
@@ -11,7 +11,6 @@ To see your past deposits, please press 3.\n
 To make a withdraw, please press 4.\n
 To see your past withdrawals, please press 5.\n
 To finish with your session, please press 7."
-
 value = gets.chomp.to_i
   if value == 1
     puts "Your current balance is $#{$balance}."
@@ -22,9 +21,37 @@ value = gets.chomp.to_i
       paid_from = gets.chomp
     puts "Please enter the date you would like to make this deposit."
       date = gets.chomp
-      newCredit = Credit.new(amount, paid_from, date)
+    puts "Please classify this deposit: P = paycheck; G = gift; L = loan; O = other."
+      classification = gets.chomp.downcase
+        if classification == "p"
+          category = "Paycheck"
+        elsif classification == "g"
+          category = "Gift"
+        elsif classification == "l"
+          category == "Loan"
+        elsif classification == "o"
+          category == "Other"
+        end
+      newCredit = Credit.new(amount, paid_from, date, category)
   elsif value == 3
     Credit.show_history
+    puts "Would you like to view your history by category? Y/N"
+      response = gets.chomp.downcase
+      if response == "y"
+        puts "Please select a category to view: P = paycheck; G = gift; L = loan; O = other."
+          response == gets.chomp.downcase
+            if response == "p"
+              Credit.credit_by_paycheck
+            elsif response == "g"
+              Credit.credit_by_gift
+            elsif response == "l"
+              Credit.credit_by_loan
+            elsif response == "o"
+              Credit.credit_by_other
+            end
+      elsif response == "n"
+        Credit.show_history
+      end
   elsif value == 4
     puts "How much would you like to withdraw?"
       amount = gets.chomp.to_i
@@ -32,9 +59,37 @@ value = gets.chomp.to_i
       paid_to = gets.chomp
     puts "Please enter the date you would like to make this withdraw."
       date = gets.chomp
-      new_debit = Debit.new(amount, paid_from, date)
+    puts puts "Please classify this withdrawal: L = living expenses; E = entertainment; S = student loan payment; O = other."
+      classification = gets.chomp.downcase
+        if classification == "l"
+          category = "Living Expenses"
+        elsif classification == "e"
+          category = "Entertainment"
+        elsif classification == "s"
+          category == "Student Loans"
+        elsif classification == "o"
+          category == "Other"
+        end
+      new_debit = Debit.new(amount, paid_from, date, category)
   elsif value == 5
       Debit.show_history
+      puts "Would you like to view your history by category? Y/N"
+        response = gets.chomp.downcase
+        if response == "y"
+          puts "Please select a category to view: L = living expenses; E = entertainment; S = student loan payment; O = other."
+            response == gets.chomp.downcase
+              if response == "l"
+                Debit.debit_by_living_expense
+              elsif response == "e"
+                Debit.debit_by_entertainment
+              elsif response == "s"
+                Debit.debit_by_loans
+              elsif response == "o"
+                Debit.debit_by_other
+              end
+        elsif response == "n"
+          Debit.show_history
+        end
   elsif value == 6
       puts "Thank you, we hope to see you again soon."
   end
--- models/finance_models.rb
+++ models/finance_models.rb
@@ -1,11 +1,41 @@
 $balance = 0
 
 class Debit
-  attr_accessor :paid_to, :amount, :date, , :category, :debits
+  attr_accessor :paid_to, :amount, :date, :category, :debits
   @@debits = []
   def Debit.show_history
-    puts "These are your past debit transactions: #{@@debits}."
+    debit_list = @@debits.map.with_index(1){|element,index| "#{index}. #{element}"}
+    puts "These are your past debit transactions: #{debit_list}."
   end
+
+  def Debit.by_living_expense
+    living_expense_transactions = @@debits.select do |debit|
+      debit.category == "Living Expenses"
+      puts living_expense_transactions
+    end
+  end
+
+  def Debit.by_entertainment
+    entertainment_transactions = @@debits.select do |debit|
+      debit.category == "Entertainment"
+      puts entertainment_transactions
+    end
+  end
+
+  def Debit.by_loans
+    loans_transactions = @@debits.select do |debit|
+      debit.category == "Student Loans"
+      puts loans_transactions
+    end
+  end
+
+  def Debit.by_other
+    other_transactions = @@debits.select do |debit|
+      debit.category == "Other"
+      puts other_transactions
+    end
+  end
+
   def initialize(amount, paid_to, date, category)
     @amount = amount
     @paid_to = paid_to
@@ -13,15 +43,46 @@ class Debit
     @category = category
     $balance = $balance - @amount
     @@debits.push(self)
-    puts "You've successfully paid $#{@amount} for #{@paid_to}. Your new balance is $ #{$balance}. You have made #{@@debits.length} debits so far. "
+    puts "You've successfully paid $#{@amount} for #{@paid_to}. The category of this deposit is #{@category}. Your new balance is $ #{$balance}. You have made #{@@debits.length} debits so far. "
   end
+
 end
+##END OF DEBIT CLASS
 
 class Credit
   attr_accessor :paid_from, :amount, :date, :category, :credits
   @@credits = []
   def Credit.show_history
-    puts "These are your past credit transactions: #{@@credits}."
+    credit_list = @@credits.map.with_index(1){|element,index| "#{index}. #{element}"}
+    puts "These are your past credit transactions: #{credit_list}."
+  end
+
+  def Credit.by_paycheck
+    paycheck_transactions = @@credits.select do |credit|
+      credit.category == "Paycheck"
+      puts paycheck_transactions
+    end
+  end
+
+  def Credit.by_gift
+    gift_transactions = @@credits.select do |credit|
+      credit.category == "Gift"
+      puts gift_transactions
+    end
+  end
+
+  def Credit.by_loan
+    loan_transactions = @@credits.select do |credit|
+      credit.category == "Loan"
+      puts loan_transactions
+    end
+  end
+
+  def Credit.by_other
+    other_transactions = @@credits.select do |credit|
+      credit.category == "Other"
+      puts other_transactions
+    end
   end
 
   def initialize(amount, paid_from, date, category)
@@ -31,6 +92,7 @@ class Credit
     @category = category
     $balance = $balance + @amount
     @@credits.push(self)
-    puts "You've successfully deposited $#{@amount} from #{@paid_from}. Your new balance is $ #{$balance}. You have made #{@@credits.length} deposit so far. "
+    puts "You've successfully deposited $#{@amount} from #{@paid_from}. The category of this credit is #{@category}. Your new balance is $ #{$balance}. You have made #{@@credits.length} deposit so far."
   end
+
 end
