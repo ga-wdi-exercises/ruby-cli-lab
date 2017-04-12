c5e6b800e1eabbf8baf1abc656b90f25d0f56b31
Caroline Chen <carolinecaroline@gmail.com>
2017-04-12 12:35:41 -0400

    add option to view all transactions

--- app.rb
+++ app.rb
@@ -24,23 +24,49 @@ class Transaction
 
 end
 
-$transactions_list = []
 
-def new_transaction
-puts "enter the payee (just the name)"
-payee = gets.chomp
-puts "enter the amount spent (just the number)"
-amount = gets.chomp.to_i
-puts "enter the date (as number mmddyy)"
-date = gets.chomp.to_i
-puts "enter the category (just the word)"
-category = gets.chomp
+class Account
+	attr_accessor :transactions, :balance
+	def initialize
+		@transactions = []
+		@balance = 0
+	end
 
-transaction_instance = Transaction.new(payee, amount, date, category)
-$transactions_list << transaction_instance
+	def menu
+		puts "Welcome.  Enter 1 to enter a new transaction.  Enter 2 to view all transactions."
+		menu_choice = gets.chomp.to_i
+		if menu_choice == 1
+			new_transaction
+		elsif menu_choice == 2
+			list_all_transactions
+		end
+	end
+
+	def new_transaction
+	puts "Enter the payee (just the name)"
+	payee = gets.chomp
+	puts "Enter the amount spent (just the number)"
+	amount = gets.chomp.to_i
+	puts "Enter the date (as number mmddyy)"
+	date = gets.chomp.to_i
+	puts "Enter the category (just the word)"
+	category = gets.chomp
+
+	transaction_instance = Transaction.new(payee, amount, date, category)
+	@transactions << transaction_instance
+	end
+
+	def list_all_transactions
+		transactions.each do |transaction|
+			puts "You paid #{transaction.payee} $#{transaction.amount} on #{transaction.date} for #{transaction.category}"
+		end
+	end
+
 end
 
+account = Account.new
 
-
-
+loop do
+	account.menu
+end
 binding.pry
