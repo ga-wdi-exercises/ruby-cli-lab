0c2de966fb9c53224015fac8b689a7aca0987f24
Erin <erinedawson@gmail.com>
2017-04-12 14:26:26 -0400

    add loop to iterate through menu.  make variables global.

--- app.rb
+++ app.rb
@@ -1,43 +1,48 @@
-require_relative "data"
+# require_relative "data"
 
-cards = data[:cards]
-deck = []
-front = []
-back = []
-correct = []
-wrong = []
-categories = []
+# cards = data[:cards]
+$deck = []
+$front = []
+$back = []
+$correct = []
+$wrong = []
+$categories = []
 
 
 class Flashcard
-  attr_accessor :id, :question, :answer
+  attr_accessor :question, :answer
 
-  def initialize (id, question, answer)
-    @id = id,
-    @question = question,
+  def initialize (question, answer)
+    @question = question
     @answer = answer
   end
 end
 
+$deck << Flashcard.new("What's the capital of Virginia?", "Richmond")
+$deck << Flashcard.new("Can birds see color?", "yes")
+$deck << Flashcard.new("Peanut butter and ___ sandwich", "jelly")
+$deck << Flashcard.new("You are a Web Development ___ student", "Immersive")
+$deck << Flashcard.new("What is the capital of France?", "Paris")
+
+
+
 puts "Welcome to Flash Card Bonanza!  What would you like to do?"
 
+loop do
 
   puts "Press 1 to create a new card. 0 to quit"
   puts "Press 2 to view all of your cards. 0 to quit"
-  # puts "Press 3 to edit a card. 0 to quit"
-  # puts "Press 4 to delete a card. 0 to quit"
-  # puts "Press 5 to view your score. 0 to quit"
-  # puts "Press 6 to view recent answers. 0 to quit"
-  # puts "Press 7 to play.  0 to quit"
+  puts "Press 3 to edit a card. 0 to quit"
+  puts "Press 4 to delete a card. 0 to quit"
+  puts "Press 5 to view your score. 0 to quit"
+  puts "Press 6 to view recent answers. 0 to quit"
+  puts "Press 7 to play.  0 to quit"
 
 
-user_input = gets.chomp.to_i
+  user_input = gets.chomp.to_i
 
-# while user_input > 0 do
 
   if (user_input == 1)
-    new_card = Flashcard.new(flashcard[:question], flashcard[:answer])
-      deck << new_card
 
     puts "Wonderful!  Enter your question"
     question = gets.chomp
@@ -46,41 +51,45 @@ user_input = gets.chomp.to_i
     answer = gets.chomp
 
 
+    new_card = Flashcard.new(question, answer)
+    $deck << new_card
+
   end
 
-if (user_input == 2)
-    deck.each do |card|
-    puts card
-    # return deck
+  if (user_input == 2)
+      $deck.each do |card|
+      puts "#{card.question} - #{card.answer}"
+    end
   end
+
+
+    if (user_input == 3)
+      $deck.map do |card|
+  #some edit functionality
+      end
+    end
+
+    if (user_input == 4)
+      $deck.delete()
+    end
+
+    if (user_input == 5)
+      puts correct.length
+    end
+
+    if (user_input == 6)
+      puts $correct
+      puts $wrong
+    end
+
+    if (user_input == 7)
+      puts card.question
+      response = gets.chomp
+      if (response == answer[0])
+        $correct << response
+      elsif
+        $wrong << response
+      end
 end
 
-
-  if (user_input == 3)
-    deck.map do |card|
-#some edit functionality
-    end
-  end
-
-  if (user_input == 4)
-    deck.delete()
-  end
-
-  if (user_input == 5)
-    puts correct.length
-  end
-
-  if (user_input == 6)
-    puts correct
-    puts wrong
-  end
-
-  if (user_input == 7)
-    puts question[0]
-    response = gets.chomp
-    if (response == answer[0])
-      correct << response
-    elsif
-      wrong << response
-    end
 end
