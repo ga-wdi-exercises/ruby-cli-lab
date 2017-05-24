5a121a86d3075a7f6271fc0986aa433894d5de17
Tim Chase <tchase4544@gmail.com>
2017-05-24 12:09:39 -0400

    fully functioning,ps. i gotta commit more

--- money.rb
+++ money.rb
@@ -1,24 +1,30 @@
 
- # see a list of all transactions
- # edit / delete transactions, and see their current balance.
- #  look at transactions by category and see the total 'cost' of transactions 
- #  support debits and credits
-#     ____                                   __   _______
-#    / __ \___  ______________  ____  ____ _/ /  / ____(_)___  ____ _____  ________
-#   / /_/ / _ \/ ___/ ___/ __ \/ __ \/ __ `/ /  / /_  / / __ \/ __ `/ __ \/ ___/ _ \
-#  / ____/  __/ /  (__  ) /_/ / / / / /_/ / /  / __/ / / / / / /_/ / / / / /__/  __/
-# /_/    \___/_/  /____/\____/_/ /_/\__,_/_/  /_/   /_/_/ /_/\__,_/_/ /_/\___/\___/
+
+ # edit / delete transactions
+
+puts " ______                                       __   _______ __ "
+puts "|   __ \  .-----.----.-----.-----.-----.---.-.|  | |    ___|__|.-----.---.-.-----.----.-----."
+puts "|    __/ |  -__|   _|__ --|  _  |     |  _  ||  | |    ___|  ||     |  _  |     |  __|  -__|"
+puts "|___|    |_____|__| |_____|_____|__|__|___._||__| |___|   |__||__|__|___._|__|__|____|_____|"
+  
+require "pry"
 class Menu
-  def self.display
+	def initialize(app)
+		@app = app
+	end
+  def display
     while 1
+    	puts ""
     	puts "Welcome to Personal Finance"
-    	puts "1 to enter Transaction"
-		puts "2 to see Transaction History"
-		puts "3 "
-		puts "4 "
-		puts "5 "
-      input = gets.chomp
-      if ["1","2","3"].include? input
+    	puts ""
+    	puts "1 to see Balance and Deposit"
+    	puts "2 to enter Transaction"
+		puts "3 to see Transaction History"
+		puts "4 See Transactions by category"
+		puts "5 Edit Transaction History"
+		puts "10 Exit"
+      input = gets.chomp!
+      if ["1","2","3","4","10"].include? input
         self.select input
         break
       else
@@ -26,42 +32,109 @@ class Menu
       end
     end
   end
-  def self.select number
+  def select number
     puts "You selected #{number}"
+    # my_finances = Tracker.new
+    case number
+    when "1"
+    	puts ""
+    	puts "You currently have $#{@app.balance}"
+    	puts "Would you like to make a deposit?"
+    	puts "type 'yes' or 'no' "
+    	depo = gets.chomp!
+    	if depo == "yes"
+    		puts ""
+    		puts "how much would you like to deposit?"
+    		moneys = gets.chomp.to_i
+    		@app.add_money(moneys)
+    		puts ""
+    		puts "You now have $#{@app.balance}"
+    		puts "Press Enter to go back to main menu"
+    		gets
+    		display
+    	else
+    		display
+    	end
+    when "2"
+    	@app.add_transaction
+    	display
+    when "3"
+    	@app.history.each do |x|
+    		puts x
+    	end
+    	display
+    when "4"
+    	puts "Type a category(food, home improvement, travel...)"
+    	@app.sorter
+    	display
+    when "5"
+    when "6"
+    when "10"
+    	return
+    end
   end
 end
 
-Menu.display
-
-require "pry"
-class Transaction
-	attr_accessor :payee,:amount,:date,:category
-
-	def initialize(payee,amount,date,category)
-		@payee, @amount, @date, @category = payee,amount,date,category
-	end
-end
-
 class Tracker
-	attr_accessor :history,:payee,:amount,:date,:category
+	attr_accessor :history
+	attr_reader :balance
 	def initialize()
-		@payee, @amount, @date, @category = payee,amount,date,category
-		@history = []
+		@balance = 100
+		@history = [
+			{:payee=>"Subway", :amount=>5, :date=>"01/01/17", :category=>"Food"},
+			{:payee=>"Fuel pizza", :amount=>12, :date=>"01/02/17", :category=>"Food"},
+			{:payee=>"Bwi", :amount=>500, :date=>"12/12/12", :category=>"Travel"}
+		]
 	end
-	def add_transaction in_payee,in_amount,in_date,in_category
+	def add_transaction
+		puts "Where did you make a purchase ?"
+		in_payee = gets.chomp.capitalize!
+		puts "How much did you spend ?"
+		in_amount = gets.chomp.to_i
+		puts "What was the date ?( DD/MM/YY )"
+		in_date = gets.chomp!
+		puts "What category does it fit? (Home Improvement,Food,Travel...)"
+		in_category = gets.chomp.capitalize!
 		@history.push({
 			payee: in_payee,amount: in_amount,date: in_date,category: in_category
 			})
+		@balance -= in_amount
+	end
+
+	def sorter
+		cat_name = gets.chomp.capitalize!
+		temp_list  = @history.select {|x| x[:category] == cat_name}
+		puts temp_list
+		puts ""
+		get_total(temp_list)
+		puts ""
+	end
+
+	def get_total hash_list
+		total = 0
+		hash_list.each do |item|
+			total += item[:amount]
+		end
+		puts "$#{total} total in this category"
+	end
+
+	def add_money dollars
+		@balance += dollars
+		positive_transaction dollars
+	end
+	def positive_transaction dollars
+		time_of = Time.now.strftime("%d/%m/%Y")
+		@history.push({payee: "Self",amount: dollars,date: time_of,category: "Deposit"})
 	end
 end
+my_finances = Tracker.new
+menu = Menu.new(my_finances)
 
-my_history = Tracker.new
-# test = Transaction.new("Lowes",250,"5/21/2017","Home Improvement")
+menu.display
 
 binding.pry
 
-sorted = my_history.history.sort_by { |k| k["date"] }
-puts sorted
+
 
 
 
