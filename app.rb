e64e3d10ef6d7817e3442bc16d5b2449bf6277cd
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-20 12:23:11 -0400

    functioning game via pry

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
+require "pry"
+
 class Battle_Board
-attr_accessor :size
+attr_accessor :board, :ship_board, :ship1, :ship2
   def initialize(size, val)
     @board = []
     @ship_board = []
@@ -8,12 +10,14 @@ attr_accessor :size
     @val = val
     @ship_val = 0
     @score = 0
+    @ship1 = []
+    @ship2 = []
   end
   def make_board
-    @size.times do |row_i|
-      row = []
-      @size.times { |column_i| row << @val } # or row.push(val)
-      @board << row # or board.push(val)
+    @size.times do |row_index|
+      @row = []
+      @size.times { |column_index| @row << @val } # or row.push(val)
+      @board << @row # or board.push(val)
     end
     @board.each do |i|
       print i
@@ -41,7 +45,7 @@ attr_accessor :size
       @score -= 1
       puts "Your score is #{@score}"
     else
-      puts " "
+      puts ""
       puts "Nice hit!"
       @score += 5
       puts "Your score is #{@score}"
@@ -50,13 +54,54 @@ attr_accessor :size
     puts "1 is untested water, 0 is a miss, and 5 is a hit."
   end
 
-  def place_ship
+  def make_ship(ship)
+    up_or_right = rand(0..1)
+    if up_or_right == 0
+      x = rand(0..9)
 
+      y = rand(0..4)
+      ship << [x, y]
+      ship << [x, y + 1]
+      ship << [x, y + 2]
+      ship << [x, y + 3]
+      ship << [x, y + 4]
+
+    else
+      y = rand(0..9)
+
+      x = rand(0..4)
+      ship << [x, y]
+      ship << [x + 1, y]
+      ship << [x + 2, y]
+      ship << [x + 3, y]
+      ship << [x + 4, y]
+
+    end
+  end
+
+  def add_ship(ship)
+    ship.each do |i|
+      y = i[0]
+      x = i[1]
+      @ship_board[x][y] = 5
+    end
+  end
 
 end
 
+
+
+
 b1 = Battle_Board.new(10, 1)
 
+
 b1.make_board
+b1.make_ship(b1.ship1)
+b1.make_ship(b1.ship2)
+b1.add_ship(b1.ship1)
+b1.add_ship(b1.ship2)
 b1.guess(1, 1)
 b1.guess(5, 7)
+
+
+binding.pry
