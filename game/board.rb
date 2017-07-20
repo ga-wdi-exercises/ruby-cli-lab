4273b685ef3be2c14ad3e9c4d6f4ff2f3f96bdcc
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-20 13:44:26 -0400

    worked on board functionality

--- game/board.rb
+++ game/board.rb
@@ -2,51 +2,104 @@ require "Matrix"
 
 class Board
 
+  WATER = "-"
+  SHIP = "S"
+  HIT = "X"
+  MISS = "O"
+
+
+  attr_reader :length, :height, :matrix
+
   def initialize m, n
     @length = m
     @height = n
-    @board = Matrix.build(m, n) {|row, col| "X" }
-    render
+    @matrix = Array.new(m) { Array.new(n, "-")}
+
   end
 
   def reset
-    @board = Matrix.build(m, n) {|row, col| "X" }
+    @matrix = Array.new(m) { Array.new(n, "-")}
   end
 
   # valid_move
   # row - the x starting location of the ship
   # column - the y starting location of the ship
   # length - the length of the ship
-  # type - the placement directions
+  # dir - the placement directions
   #   0 if horizontal
   #   1 if vertical
-  def valid_move? row, column, len, type
+  def valid_placement? row, col, ship_len, dir
     # error check type = 0 or 1 only
-    if type == 0
-      if row + len > @length
+    if dir == 0
+      if col + ship_len > @length
         return false
       end
-    elsif type == 1
-      if col + len > @height
+    elsif dir == 1
+      if row + ship_len > @height
         return false
       end
     else
-      puts "not a valid direction"
+      # "not a valid direction"
       return false
     end
     return true
   end
 
-
-  def render
-    i = 0
-    @board.each do |number|
-      print number.to_s + " "
-      i+= 1
-      if i == @board.column_size
-        print "\n"
-        i = 0
+  # add_ship
+  # row - the x starting location of the ship
+  # column - the y starting location of the ship
+  # len = length of the ship
+  # dir = the orientation (0 for hor, 1 vert)
+  def add_ship row, col, len, dir
+    i = len
+    if dir == 0
+      while i > 0
+        @matrix[row][col] = "S"
+        col += 1
+        i -= 1
+      end
+    else
+      while i > 0
+        @matrix[row][col] = "S"
+        row += 1
+        i -= 1
       end
     end
   end
+
+  # returns the item at matrix row, col
+  def get_position row, col
+    return @matrix[row][col]
+  end
+
+  # sets the matrix[row][col] to str
+  def set_position row, col, str
+     @matrix[row][col] = str
+  end
+
+  # renders the board to the user
+  def render
+    puts to_s
+  end
+
+  def to_s
+    string = ""
+    i = 0
+    @matrix.each do |item|
+      if [WATER, SHIP].include?(item)
+        string += "#{WATER} ".to_str
+      else
+        string += item.to_s + " "
+      end
+      i+= 1
+      if i == @length
+        string += "\n"
+        i = 0
+      end
+    end
+
+    puts string
+    string
+  end
+
 end
