ddc1e0c8605163d612fff3d07c154119612997b9
Admin <Tgafvert@gmail.com>
2017-07-20 16:43:39 -0400

    all doneskies

--- battleship.rb
+++ battleship.rb
@@ -4,13 +4,28 @@ class BattleShips
 def self.initialize_board(n, val)
   Array.new(n) { Array.new(n) { val } }
 end
-
+@hitPoints = 10
 @shipCount = 0
 @shipCollide = false
 @boardShow = initialize_board(10, "~~")
+@boardLogic = initialize_board(10, "~~")
 
 def self.drawBoard
+  puts "[]x0x1x2x3x4x5x6x7x8x9"
+  yCount = 0
 @boardShow.each do |arr|
+  print "y"+yCount.to_s
+  yCount +=1
+ arr.each do |subarr|
+   print subarr
+ end
+ puts "\n"
+end
+ puts "[]x0x1x2x3x4x5x6x7x8x9"
+  yCount = 0
+@boardLogic.each do |arr|
+  print "y"+yCount.to_s
+  yCount +=1
  arr.each do |subarr|
    print subarr
  end
@@ -39,7 +54,7 @@ def self.buildShip (y, x, orient)
         if @shipCollide == false
             i=0
            until i > 4
-           @boardShow[@y + i][@x] = "xx"
+           @boardLogic[@y + i][@x] = "XX"
            i+=1
            end
            @shipCount +=1
@@ -49,7 +64,7 @@ def self.buildShip (y, x, orient)
         if @shipCollide == false
            i = 0
            until i > 4
-           @boardShow[@y - i][@x] = "xx"
+           @boardLogic[@y - i][@x] = "XX"
            i+=1
            end
            @shipCount +=1
@@ -61,7 +76,7 @@ def self.buildShip (y, x, orient)
            if @shipCollide == false
              i=0
              until i > 4
-             @boardShow[@y][@x + i] = "XX"
+             @boardLogic[@y][@x + i] = "XX"
              i+=1
              end
              @shipCount +=1
@@ -71,7 +86,7 @@ def self.buildShip (y, x, orient)
            if @shipCollide == false
              i=0
              until i > 4
-             @boardShow[@y][@x - i] = "XX"
+             @boardLogic[@y][@x - i] = "XX"
              i+=1
              end
              @shipCount +=1
@@ -89,43 +104,73 @@ def self.checkCollision(direction)
   if dir == "down"
         i = 0
         until i > 4
-        if @boardShow[@y + i][@x] != "~~"
+        if @boardLogic[@y + i][@x] != "~~"
         @shipCollide = true
+
         end
         i+=1
     end
   elsif dir == "up"
        i = 0
        until i > 4
-       if @boardShow[@y - i][@x] != "~~"
+       if @boardLogic[@y - i][@x] != "~~"
        @shipCollide = true
+
        end
        i+=1
     end
   elsif dir =="right"
       i = 0
       until i > 4
-       if @boardShow[@y][@x + 1] != "~~"
+         if @boardLogic[@y][@x + i] != "~~"
        @shipCollide = true
-      end
+
+        end
       i+=1
-    end
+      end
   elsif dir =="left"
         i = 0
        until i > 4
-       if @boardShow[@y][@x - 1] != "~~"
-       @shipCollide = true
-       end
+         if @boardLogic[@y][@x - i] != "~~"
+         @shipCollide = true
+
+         end
        i+=1
-     end
+        end
   end
 end
 
 while @shipCount < 2 do
   placeShip
-  puts @shipCount
+end
+puts "Enter X and Y coordinates like this '1,2' for x1 and y2"
+while @hitPoints != 0
+drawBoard
+puts "Enter X and Y coordinates"
+
+@coordXY = []
+gets.chomp.split(/\s*,\s*/).each { |item|
+   if item =~ /^\s*(\d+)\s*$/
+     @coordXY << item.to_i
+   else
+     abort "FAILURE TO FOLLOW INSTRUCTIONS: #{item}"
+   end
+ }
+if @boardLogic[@coordXY[1]][@coordXY[0]] == "XX"
+    if @boardShow[@coordXY[1]][@coordXY[0]] != "XX"
+  @hitPoints -= 1
+  puts "HIT!"
+  @boardShow[@coordXY[1]][@coordXY[0]] = @boardLogic[@coordXY[1]][@coordXY[0]]
 end
 
-drawBoard
-# placeShip
+else
+  puts @coordXY
+  puts "MISS!"
+  puts @boardShow[@coordXY[1]][@coordXY[0]]
+  if @boardShow[@coordXY[1]][@coordXY[0]] == "~~"
+  @boardShow[@coordXY[1]][@coordXY[0]] = "()"
+end
+end
+end
+puts "YOU WIN"
 end
