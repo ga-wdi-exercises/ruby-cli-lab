8a10a876dc6f439d53e31c67bd08d98c1c57d920
Luis Wiese <luiswiese@gmail.com>
2017-07-21 13:59:37 -0400

    personal finance another_transaction

--- personal_finance/bank.rb
+++ personal_finance/bank.rb
@@ -1,3 +1,5 @@
+puts "Welcome to Wiese Bank #{@clients}"
+puts "How may i help you?"
 
 class Accounts
   attr_accessor :clients, :balance
@@ -11,25 +13,57 @@ class Accounts
   end
 
   def transactions
-       puts "Welcome to Wiese Bank #{@clients}"
-        puts "How may i help you?"
-        puts "Would you like to make a withdrawl(w),deposit(d) or see your balance(b)?"
+        puts "Would you like to make a withdrawl(w),deposit(d), see your balance(b) or cancel(c)?"
         @type = gets.chomp
-      case @type
-        when "w"
+        case @type
+          when "w"
           puts "you current balance is #{@balance}"
           puts "Please enter amount"
           amount = gets.chomp.to_i
           withdrawl(amount)
-        when "d"
+          when "d"
           puts "you current balance is #{@balance}"
           puts "Please enter amount"
           amount = gets.chomp.to_i
           deposit(amount)
-        when "b"
+          when "b"
           puts "you current balance is #{@balance}"
-      end
-      transactions #will call the method and will run ultil the dawn
+          when "c"
+          puts "Thank you for making bussines with us"
+          puts "Good bye"
+        end
+
+  def another_transaction
+        puts "Would you like to make another transactions yes (y) or no (n)"
+        @type2 = gets.chomp
+        case @type2
+          when "y"
+          puts "Would you like to make a withdrawl(w),deposit(d), see your balance(b) or cancel(c)?"
+            @type3 = gets.chomp
+            case @type3
+              when "w"
+                puts "you current balance is #{@balance}"
+                puts "Please enter amount"
+                amount = gets.chomp.to_i
+                withdrawl(amount)
+                when "d"
+                puts "you current balance is #{@balance}"
+                puts "Please enter amount"
+                amount = gets.chomp.to_i
+                deposit(amount)
+                when "b"
+                puts "you current balance is #{@balance}"
+                when "c"
+                puts "Thank  you for making bussines with us"
+                puts "Good bye"
+            end
+          when "n"
+          puts "Thank  you for making bussines with us"
+          puts "Good bye"
+        end
+  end
+      another_transaction
+      transactions #will call the method and will run ultil dawn
   end
   def deposit(amount)
      @balance += amount
